40a84890c53c1676e9f40316d32b4ab800547edc
Bal Kishan <70845244+bkunsolved@users.noreply.github.com>
2021-04-18 21:46:51 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,30 @@
+// jshint esversion:6
+
+const express = require("express");
+const https = require("https");
+const bodyParser = require("body-parser");
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+app.get("/", function(req, res) {
+   res.sendFile(__dirname + "/index.html");
+});
+app.post("/", function(req, res) {
+    const query = req.body.cityName;
+    const apiKey = "232c93465abdfa5f3da11c920b8839f4";
+    const unit = "metric";
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey + "&units=" + unit;
+    https.get(url, function(response) {
+        response.on("data", function(data) {
+            const weatherData = JSON.parse(data);
+            const temp = weatherData.main.temp;
+            const weatherDescription = weatherData.weather[0].description;
+            const icon = weatherData.weather[0].icon;
+            const imageURL = " http://openweathermap.org/img/wn/" + icon + "@2x.png";
+            res.write("<P>The weather is currently" + weatherDescription + "</p>");
+            res.write("<h1>The temperature in " + query + " is " + temp + " degree Celcius.</h1>");
+            res.write("<img src = "  + imageURL + ">");
+            res.send();
+        });
+    });
+});
+app.listen(3000);
